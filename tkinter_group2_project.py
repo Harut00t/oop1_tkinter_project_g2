988d28bbf512aee5ae207c347ec3830910307c05
Seiichi Aiko <seiic@amanuel-XPS.RCII.com>
2025-05-28 13:37:44 +0800

    I modified the code

--- tkinter_group2_project.py
+++ tkinter_group2_project.py
@@ -6,25 +6,36 @@ class MyApp:
         self.parent = parent
 
         self.main_frame = Frame(parent)
-        self.main_frame.pack()
+        self.main_frame.pack(expand=True)
 
-        self.button = Button(self.main_frame, text="Quit1", command=self.terminate_program)
-        self.button.pack(side="left")
-        self.button = Button(self.main_frame, text="Quit2", command=self.terminate_program)
-        self.button.pack(side="left")
+        label = Label(self.main_frame, text="Hello, Everyone!", font=("Arial", 16))
+        label.pack()
+        label = Label(self.main_frame, text="This is the widgets of Tkinter:", font=("Arial", 13))
+        label.pack()
 
-        self.bottomframe = Frame(self.main_frame)
-        self.bottomframe.pack()
+        self.button = Button(self.main_frame, text="Frame", font=("Arial", 20), padx=20, pady=10, command=self.open_frame)
+        self.button.pack(side="left", pady=10)
+        self.button = Button(self.main_frame, text="Label", font=("Arial", 20), padx=20, pady=10)
+        self.button.pack(side="left", pady=10)
+        self.button = Button(self.main_frame, text="Button", font=("Arial", 20), padx=20, pady=10)
+        self.button.pack(side="left", pady=10)
+        self.button = Button(self.main_frame, text="Button checker", font=("Arial", 20), padx=20, pady=10)
+        self.button.pack(side="left", pady=10)
 
-        self.button = Button(self.bottomframe, text="Quit3", command=self.terminate_program)
-        self.button.pack(side="top")
-        self.button = Button(self.bottomframe, text="Quit4", command=self.terminate_program)
-        self.button.pack(side="left")
+    def open_frame(self):
+        new_window = Frame(self.parent)
+        new_window.title("Frame")
+        new_window.pack(expand=True)
 
-    def terminate_program(self):
-        self.parent.destroy()
+        label = Label(new_window, text="This is the new screen!", font=("Arial", 16))
+        label.pack(pady=20)
+
+        close_button = Button(new_window, text="Close", font=("Arial", 14), command=new_window.destroy)
+        close_button.pack()
+
 
 if __name__ == "__main__":
     root = Tk()
+    root.state("zoomed")
     myapp = MyApp(root)
     root.mainloop()
